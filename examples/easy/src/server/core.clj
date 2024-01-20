85e612711c17afe806bf40f2e274dc061915c9ad
Brent Soles <17535379+bnert@users.noreply.github.com>
2024-01-19 21:53:20 -0500

    add service middleware

--- examples/easy/src/server/core.clj
+++ examples/easy/src/server/core.clj
@@ -3,9 +3,40 @@
     [examples.common.lacinia-engine.star-wars.resolvers :as ex.resolvers]
     [examples.common.core :as ex.core]
     [graphql.kit :as kit]
-    [manifold.executor :as m.e]))
+    [manifold.executor :as m.e]
+    [taoensso.timbre :refer [info]]))
 
 (def port 9112)
+
+; TODO: Figure out better examples (i.e. auth(n,z), query complexity, db, CORS)
+(defn logger [message]
+  (fn [handler]
+    (fn
+      ([req]
+       (info message "I'm a logger, fear my axe...")
+       (handler req))
+      ([req res raise]
+       (info message "I'm a logger, fear my axe at some point in the future...")
+       (handler req res raise)))))
+
+(defn health-check [endpoint]
+  (let [health? #(and (= :get (:request-method %))
+                      (= endpoint (:uri %)))
+        ok-resp {:status  200
+                 :headers {"content-type" "text/plain"}
+                 :body    "ok"}]
+    (fn [handler]
+      (fn
+        ([req]
+         (info "health sync")
+         (if (health? req)
+           ok-resp
+           (handler req)))
+        ([req res raise]
+         (info "health async")
+         (if (health? req)
+           (res ok-resp)
+           (handler req res raise)))))))
 
 (defn -main [& _args]
   (kit/service!
@@ -15,6 +46,13 @@
      :endpoints {:graphiql  "/graphiql"
                  :http      "/graphql"
                  :websocket "/graphql/subscribe"}
+     ; Underlying, there is a default "middleware chain". The main responsibility
+     ; is to parse query params and json, as well as format responses.
+     ;
+     ; The resulting middleware will look like
+     ; logger -> parsing ->  logger -> health-check -> root-handler
+     :middleware {:prepend [(logger "PREPEND>")]
+                  :append  [(logger "APPEND>") (health-check "/health")]}
      :scalars   ex.core/scalars
      :resolvers {:query
                  {:Mutation/addDroid ex.resolvers/add-droid
@@ -27,7 +65,7 @@
                   :Query/heros       ex.resolvers/heros}
                  :subscription
                  {:Subscription/events ex.resolvers/events}}
-     :server    {:port port}
+     :server    {:port port, #_#_:async? true}
      :schema    {:resource "graphql/schema/star-wars.edn"}
      :options   {:executor (m.e/execute-pool)}}))
 
--- src/graphql/kit/services/aleph.clj
+++ src/graphql/kit/services/aleph.clj
@@ -10,18 +10,36 @@
     [muuntaja.middleware :as m.mw]
     [taoensso.timbre :refer [info]]))
 
-; for AoT
+; delay is for AoT
 (def kit-config
   (delay
     {:graphql.kit/engine (kit.engine/engine!)
      :graphql.kit/loader (kit.loader/loader!)}))
 
+(defn handler? [lut req]
+  (get-in lut [(:request-method req) (:uri req)]))
+
+(defn service-handler  [lut]
+  (fn service-handler
+    ([req]
+     (if-let [h? (handler? lut req)]
+       (h? req)
+       {:status 404}))
+    ([req res raise]
+     (if-let [h? (handler? lut req)]
+       (h? req res raise)
+       (res {:status 404})))))
+
 (defn service! [opts]
-  (let [opts' (dissoc opts :server :endpoints :graphiql)
+  (let [opts' (dissoc opts :endpoints :graphiql :middleware :server)
         {:keys [graphiql http websockets]
          :or   {graphiql   nil #_"/graphiql"
                 http       "/graphql"
                 websockets "/graphql/subscribe"}} (:endpoints opts)
+        {:keys [append prepend]
+         :or   {append  []
+                prepend []}} (:middleware opts)
+
         ; --
         http* (kit.http/handler (into @kit-config opts'))
         ws*   (kit.ws/handler (into @kit-config opts'))
@@ -32,25 +50,22 @@
                 websockets (assoc-in [:get websockets] ws*)
                 graphiql   (assoc-in [:get graphiql] ide*))
         ; --
+                ; [inner ... outer]
+                ; [append ... prepend]
+        chain   (as-> [m.mw/wrap-params #(m.mw/wrap-format % m.core/instance)] $
+                 ; need to reverse, here, otherwise append middleware will
+                 ; be applied right to left, not left to right.
+                 (into (vec (reverse append)) $)
+                 (into $ prepend))
+        ; compiles all the middleware into a single chain
+        root-handler (reduce #(%2 %1) (service-handler lut) chain)
+        ; --
         server-opts (into {:port 9109} (:server opts))]
     ; TODO: better log message
     (info "Serving graphql.kit on:" (-> server-opts :port))
     (aleph/start-server
-      (-> (fn service-handler
-            ([req]
-             (if-let [h? (get-in lut
-                                 [(:request-method req)
-                                  (:uri req)])]
-               (h? req)
-               {:status 404}))
-            ([req res _raise]
-             (res
-               (if-let [h? (get-in lut
-                                   [(:request-method req)
-                                    (:uri req)])]
-                 (h? req)
-                 {:status 404}))))
-          (m.mw/wrap-params)
-          (m.mw/wrap-format m.core/instance))
+      (cond-> root-handler
+        (:async? server-opts)
+          aleph/wrap-ring-async-handler)
       server-opts)))
 
